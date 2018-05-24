b31135488feb0dcf35fa381aba381142849eb0df
vendys-huiyong <hui@vendys.co.kr>
2018-05-24 12:25:54 +0900

    - timer2 예제

--- Timer/App.js
+++ Timer/App.js
@@ -10,61 +10,63 @@ import {
   StyleSheet,
   Text,
   View,
-  Button
+  Button,
+  TouchableOpacity,
+  Image
 } from 'react-native';
 
-// 긁어옴, 공백채워주기
-function leadingZeros(n, digits) {
-  var zero = '';
-  n = n.toString();
 
-  if (n.length < digits) {
-    for (i = 0; i < digits - n.length; i++)
-      zero += '0';
-  }
-  return zero + n;
-}
 
-// 긁어옴, 날짜 포맷 처리
-function getDateText() {
-  var d = new Date();
-  return leadingZeros(d.getHours(),2) + ":" + leadingZeros(d.getMinutes(),2) + ":" + leadingZeros(d.getSeconds(),2);
-}
 
-// 긁어옴, 랜덤 색상
-function randomColor() {
-  return 'rgb(' + (Math.floor(Math.random() * 256)) + ',' + (Math.floor(Math.random() * 256)) + ',' + (Math.floor(Math.random() * 256)) + ')';
-}
-
-/*
-  visual code에서 아래 에러는 도대체 왜 발생하는것인가.
-    [js] 'type aliases'는 .ts 파일에서만 사용할 수 있습니다.
-    type Props = {}
-*/
 type Props = {};
 export default class App extends Component<Props> {
+
+  leadingZeros(n, digits) {
+    var zero = '';
+    n = n.toString();
+    if (n.length < digits) {
+      for (i = 0; i < digits - n.length; i++) { zero += '0';}
+    }
+    return zero + n;
+  }
+  
+  getDateText() { var d = new Date(); return this.leadingZeros(d.getHours(),2) + ":" + this.leadingZeros(d.getMinutes(),2) + ":" + this.leadingZeros(d.getSeconds(),2); }
+  randomColor() { return 'rgb(' + (Math.floor(Math.random() * 256)) + ',' + (Math.floor(Math.random() * 256)) + ',' + (Math.floor(Math.random() * 256)) + ')'; }
+  
 
   constructor(props) {
     super(props)
     
+    this.changeBackground = this.changeBackground.bind(this);
+
     // state 랜덤 초기화
     this.state = { 
-      timeText : getDateText(),
-      touchColor : randomColor()
+      timeText : this.getDateText(),
+      touchColor : 'cadetblue',
+      isShowPalette : false
     };
     
     // 1초당 타이머 시작 , setState로 rerender
     setInterval(()=>{
       this.setState( previousState => {
-        return {timeText : getDateText() };
+        return {timeText : this.getDateText() };
       })
     },1000)
   }
 
   // 내부 람다 정의
-  _onPress = () => {
+  changeBackground (color) {
     this.setState(previousState => {
-      return {touchColor : randomColor()};
+      return {
+        touchColor : color,
+        isShowPalette : !this.state.isShowPalette
+      };
+    })
+  }
+
+  togglePalette = () => {
+    this.setState(previousState => {
+      return { isShowPalette : !this.state.isShowPalette};
     })
   }
 
@@ -72,13 +74,32 @@ export default class App extends Component<Props> {
 
     return (
       <View style={ [styles.container , {backgroundColor: this.state.touchColor}] }>
-      
-        {/* 시간 출력 */}
-        <Text style={styles.welcome}> {this.state.timeText} </Text>
 
-        {/* 터치시 백그라운드 색상 랜덤 처리 */}
-        <Button onPress={this._onPress} title={"touch"} />
-      
+        {/* 팔레트 show/hide 버튼 */}
+        <View style={ [styles.button , { position:'absolute', top:30 , right:30 , width:30, height:30 }] }>
+          <TouchableOpacity  onPress={this.togglePalette}>
+            <Image style={{width:30, height:30 }} source={ require("./palette.png") }/>
+          </TouchableOpacity>
+        </View>
+
+        {/* 팔레트 */}
+        { this.state.isShowPalette &&         
+          <View style={ [styles.palette , { position:'absolute', top:80 , right:30 } ]  }>
+            <TouchableOpacity onPress={()=> {this.changeBackground('cadetblue')} }    style={ [styles.paletteItem, {backgroundColor:'cadetblue'}] } />
+            <TouchableOpacity onPress={()=> {this.changeBackground('blueviolet')} }   style={[styles.paletteItem, {backgroundColor:'blueviolet' }]} />
+            <TouchableOpacity onPress={()=> {this.changeBackground('cornflowerblue')} }   style={[styles.paletteItem, {backgroundColor:'cornflowerblue' }]} />
+            <TouchableOpacity onPress={()=> {this.changeBackground('bisque')} }     style={[styles.paletteItem, {backgroundColor:'bisque' }]} />
+            <TouchableOpacity onPress={()=> {this.changeBackground('cornsilk')} }   style={[styles.paletteItem, {backgroundColor:'cornsilk' }]} />
+            <TouchableOpacity onPress={()=> {this.changeBackground('crimson')} }    style={[styles.paletteItem, {backgroundColor:'crimson' }]} />
+            <TouchableOpacity onPress={()=> {this.changeBackground('gainsboro')} }  style={[styles.paletteItem, {backgroundColor:'gainsboro' }]} />
+            <TouchableOpacity onPress={()=> {this.changeBackground('ivory')} }      style={[styles.paletteItem, {backgroundColor:'ivory' }]} />
+            <TouchableOpacity onPress={()=> {this.changeBackground('honeydew')} }   style={[styles.paletteItem, {backgroundColor:'gray' }]} />
+          </View>
+        }
+
+        {/* 시간 출력 */}
+        <Text style={styles.timeText}> {this.state.timeText} </Text>
+
       </View>
     );
   }
@@ -91,9 +112,31 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     backgroundColor: '#F5FCFF',
   },
-  welcome: {
-    fontSize: 80,
+  timeText: {
+    fontSize: 50,
     textAlign: 'center',
     margin: 10,
-  }
+  },
+  palette: {
+    flex:1,
+    flexDirection:'row',
+    backgroundColor: 'black',
+    padding : 1
+  },
+  paletteItem: {
+    width:30 , 
+    height:30 , 
+    margin:1
+  },
+  button: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#859a9b',
+    borderRadius: 20,
+    padding: 10,
+    shadowOffset: { width: 0, height: 5 },
+    shadowRadius: 10,
+    shadowOpacity: 0.35,
+  },
 });
