dad6164f9ff266b25c6acd8c9de9f1543e05b446
jackdaw <>
2018-05-31 23:13:57 +0900

    - palette fade in/out animation

--- Timer/App.js
+++ Timer/App.js
@@ -12,7 +12,9 @@ import {
   View,
   Button,
   TouchableOpacity,
-  Image
+  Image,
+  Animated,
+  Easing
 } from 'react-native';
 
 import { StackNavigator } from 'react-navigation'
@@ -20,15 +22,13 @@ import { StackNavigator } from 'react-navigation'
 
 class MainScreen extends Component {
   static navigationOptions = {
-    title: 'Welcome',
+    title: '아아 페이스북',
   };
   render() {
-    // const { navigate } = this.props.navigation;
     return (
       <Button
-        title="Go to Jane's profile"
+        title="아아 가이드 문서 담당자여"
         onPress={() =>
-          //navigate('Timer', { name: 'Jane' })
           this.props.navigation.navigate('Timer')
         }
       />
@@ -54,19 +54,23 @@ class TimerScreen extends Component {
   getDateText() { var d = new Date(); return this.leadingZeros(d.getHours(),2) + ":" + this.leadingZeros(d.getMinutes(),2) + ":" + this.leadingZeros(d.getSeconds(),2); }
   randomColor() { return 'rgb(' + (Math.floor(Math.random() * 256)) + ',' + (Math.floor(Math.random() * 256)) + ',' + (Math.floor(Math.random() * 256)) + ')'; }
   
+  
 
   constructor(props) {
     super(props)
     
     this.changeBackground = this.changeBackground.bind(this);
-
+    this.isShowPalette = false;
+    this.paletteOpacity = new Animated.Value(0);
     // state 랜덤 초기화
     this.state = { 
       timeText : this.getDateText(),
       touchColor : 'cadetblue',
-      isShowPalette : false
+      paletteOpacity : new Animated.Value(0)
     };
     
+
+
     // 1초당 타이머 시작 , setState로 rerender
     setInterval(()=>{
       this.setState( previousState => {
@@ -79,20 +83,28 @@ class TimerScreen extends Component {
   changeBackground (color) {
     this.setState(previousState => {
       return {
-        touchColor : color,
-        isShowPalette : !this.state.isShowPalette
+        touchColor : color
       };
     })
+
+    this.togglePalette();
   }
 
   togglePalette = () => {
-    this.setState(previousState => {
-      return { isShowPalette : !this.state.isShowPalette};
-    })
+    
+    var to = (this.isShowPalette == false)? 1 : 0;
+    this.isShowPalette = !this.isShowPalette;
+    console.log(to);
+    Animated.timing(
+      this.state.paletteOpacity,{
+        toValue : to,
+        duration: 300,
+        easing : Easing.linear
+      }).start(); 
   }
 
   render() {
-
+    let { paletteOpacity } = this.state;
     return (
       <View style={ [styles.container , {backgroundColor: this.state.touchColor}] }>
 
@@ -104,19 +116,17 @@ class TimerScreen extends Component {
         </View>
 
         {/* 팔레트 */}
-        { this.state.isShowPalette &&         
-          <View style={ [styles.palette , { position:'absolute', top:80 , right:30 } ]  }>
-            <TouchableOpacity onPress={()=> {this.changeBackground('cadetblue')} }    style={ [styles.paletteItem, {backgroundColor:'cadetblue'}] } />
-            <TouchableOpacity onPress={()=> {this.changeBackground('blueviolet')} }   style={[styles.paletteItem, {backgroundColor:'blueviolet' }]} />
-            <TouchableOpacity onPress={()=> {this.changeBackground('cornflowerblue')} }   style={[styles.paletteItem, {backgroundColor:'cornflowerblue' }]} />
-            <TouchableOpacity onPress={()=> {this.changeBackground('bisque')} }     style={[styles.paletteItem, {backgroundColor:'bisque' }]} />
-            <TouchableOpacity onPress={()=> {this.changeBackground('cornsilk')} }   style={[styles.paletteItem, {backgroundColor:'cornsilk' }]} />
-            <TouchableOpacity onPress={()=> {this.changeBackground('crimson')} }    style={[styles.paletteItem, {backgroundColor:'crimson' }]} />
-            <TouchableOpacity onPress={()=> {this.changeBackground('gainsboro')} }  style={[styles.paletteItem, {backgroundColor:'gainsboro' }]} />
-            <TouchableOpacity onPress={()=> {this.changeBackground('ivory')} }      style={[styles.paletteItem, {backgroundColor:'ivory' }]} />
-            <TouchableOpacity onPress={()=> {this.changeBackground('honeydew')} }   style={[styles.paletteItem, {backgroundColor:'gray' }]} />
-          </View>
-        }
+        <Animated.View style={ [styles.palette , { position:'absolute', top:80 , right:30 , opacity:paletteOpacity } ]  }>
+          <TouchableOpacity onPress={()=> {this.changeBackground('cadetblue')} }    style={ [styles.paletteItem, {backgroundColor:'cadetblue'}] } />
+          <TouchableOpacity onPress={()=> {this.changeBackground('blueviolet')} }   style={[styles.paletteItem, {backgroundColor:'blueviolet' }]} />
+          <TouchableOpacity onPress={()=> {this.changeBackground('cornflowerblue')} }   style={[styles.paletteItem, {backgroundColor:'cornflowerblue' }]} />
+          <TouchableOpacity onPress={()=> {this.changeBackground('bisque')} }     style={[styles.paletteItem, {backgroundColor:'bisque' }]} />
+          <TouchableOpacity onPress={()=> {this.changeBackground('cornsilk')} }   style={[styles.paletteItem, {backgroundColor:'cornsilk' }]} />
+          <TouchableOpacity onPress={()=> {this.changeBackground('crimson')} }    style={[styles.paletteItem, {backgroundColor:'crimson' }]} />
+          <TouchableOpacity onPress={()=> {this.changeBackground('gainsboro')} }  style={[styles.paletteItem, {backgroundColor:'gainsboro' }]} />
+          <TouchableOpacity onPress={()=> {this.changeBackground('ivory')} }      style={[styles.paletteItem, {backgroundColor:'ivory' }]} />
+          <TouchableOpacity onPress={()=> {this.changeBackground('honeydew')} }   style={[styles.paletteItem, {backgroundColor:'gray' }]} />
+        </Animated.View>
 
         {/* 시간 출력 */}
         <Text style={styles.timeText}> {this.state.timeText} </Text>
